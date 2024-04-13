c02de7a9ab059dc71aba1a4427f7be5ea4e83825
ayazahmed07 <87937963+ayazahmed07@users.noreply.github.com>
2024-04-13 23:49:25 +0500

    updated real time coversion

--- curcov.js
+++ curcov.js
@@ -1,37 +1,56 @@
+import axios from "axios";
+import chalk from "chalk";
 import inquirer from "inquirer";
-const currency = {
-    EUR: 0.91,
-    USD: 1,
-    GBP: 0.76,
-    INR: 74.57,
-    PKR: 280,
-};
-let useranswer = await inquirer.prompt([
-    {
-        name: "from",
-        type: "list",
-        message: "Enter from currency",
-        choices: ["EUR", "USD", "GBP", "INR", "PKR"],
-    },
-    {
-        name: "to",
-        type: "list",
-        message: "Enter to currency",
-        choices: ["EUR", "USD", "GBP", "INR", "PKR"],
-    },
-    {
-        name: "amount",
-        type: "number",
-        message: "Please enter your amount",
-        choices: ["EUR", "USD", "GBP", "INR", "PKR"],
-    },
-]);
-let fromamount = currency[useranswer.from];
-let toamount = currency[useranswer.to];
-let amount = useranswer.amount;
-let baseamount = amount / fromamount;
-let convertedamount = baseamount * toamount;
-console.log(convertedamount);
-console.log(fromamount);
-console.log(toamount);
-console.log(amount);
+const API_KEY = "c6a292310837471588429a6b326972ca";
+const BASE_URL = `https://openexchangerates.org/api/latest.json?app_id=${API_KEY}`;
+async function getExchangeRates() {
+    try {
+        const response = await axios.get(BASE_URL);
+        return response.data.rates;
+    }
+    catch (error) {
+        console.error("Error fetching exchange rates:", error);
+        return {};
+    }
+}
+async function main() {
+    const exchangeRates = await getExchangeRates();
+    const currency = {
+        Euro: exchangeRates.EUR,
+        United_States_Dollar: exchangeRates.USD,
+        British_Pound_Sterling: exchangeRates.GBP,
+        Indian_Rupee: exchangeRates.INR,
+        Pakistani_Rupee: exchangeRates.PKR,
+        Japanese_Yen: exchangeRates.JPY,
+        Chiness_Yuan: exchangeRates.CHY,
+        New_Zealand_Dollar: exchangeRates.NZD,
+        South_African_Rand: exchangeRates.ZAR,
+        Mexican_Peso: exchangeRates.MXN,
+    };
+    const userAnswer = await inquirer.prompt([
+        {
+            name: "from",
+            type: "list",
+            message: chalk.blue("Enter from currency"),
+            choices: Object.keys(currency),
+        },
+        {
+            name: "to",
+            type: "list",
+            message: chalk.green("Enter to currency"),
+            choices: Object.keys(currency),
+        },
+        {
+            name: "amount",
+            type: "input",
+            message: chalk.yellow("Please enter your amount")
+        },
+    ]);
+    const fromAmount = currency[userAnswer.from];
+    const toAmount = currency[userAnswer.to];
+    const amount = (userAnswer.amount);
+    const baseAmount = amount / fromAmount;
+    const convertedAmount = baseAmount * toAmount;
+    console.log("Converted amount:", convertedAmount);
+}
+main();
